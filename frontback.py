dc2c1f3b2773deb6e69f803e7dea32384cc74e27
Prashant Rana Magar <ranamagar.prashant@gamil.com>
2022-07-24 17:25:16 +0545

    frontback

---
+++ frontback.py
@@ -0,0 +1,17 @@
+
+# Given a string, return a new string where the first and last chars have been exchanged.
+
+
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
+
+def front_back(str):
+  if len(str) <= 1:
+    return str
+  
+  mid = str[1:len(str)-1]  # can be written as str[1:-1]
+  
+  # last + mid + first 
+  #last can be  written as str[-1]
+  return str[len(str)-1] + mid + str[0]
