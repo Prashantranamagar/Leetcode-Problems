95af3cb5b473dc2b21cf1c947499320509d90874
Prashant Rana Magar <ranamagar.prashant@gamil.com>
2022-08-22 14:09:15 +0545

    updated search in rotated array

--- Arrays/searrchInRotatedSortedArray.js
+++ Arrays/searrchInRotatedSortedArray.js
@@ -12,36 +12,52 @@ Output: -1
 Input: nums = [1], target = 0
 Output: -1
 
+Solution:
+as it is a rotated sorted array, there is a pivot. we don't know where the pivot is. when we pick the middle, either it's
+left side will be sorted or right side will be sorted. first, let's check - if left side is sorted or not. When will be 
+the left side sorted? when low<=mid; if it's sorted, we can apply binary search in it.  But, if left side of the middle 
+is sorted we will have to check if our target element is existing there or not. For that, left should be lower or equal 
+to target and mid should be higher or equal to target. if that's not the case, our target element is not there, we have 
+check into other half.
 
-Time complexity=logn
+Time complexity=log(n)
  */
 
 
 var search = function(nums, target) {
-    //use binary search
+    
     let left = 0
     let right = nums.length-1
     while(left<=right){
         let middle = Math.floor((left+right)/2)
 
+        //checking if mid is equal to target; if it is, return mid;
         if (nums[middle]==target) return middle
+			
+        //check if the left part is sorted
         if (nums[left]<=nums[middle]){
-            if(target<nums[left] || target>nums[middle]){
-                left = middle+1
-            }else{
+            //then check if target element is there or not
+            if(nusm[left]<=target && nums[middle]>=target){
                 right = middle-1
             }
-        }else{
+            // if not then we have to find target into the other half, low will mid + 1
+            else{
+                left = middle+1
+            }
+        }
+        //if left side is not sorrted right part is sorted
+        else{
             
-            if(target > nums[right] || target < nums[middle]){
-                right = middle - 1
+            if(nums[middle]<= target && nums[right] >= target){
+                right = middle + 1
             } 
             else{
-                left = middle + 1
+                left = middle - 1
   
             } 
         }
     }
+    //if we dont find the element return -1
     return -1
 };
 
