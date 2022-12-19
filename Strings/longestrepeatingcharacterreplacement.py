d9d95420e8cd55de1ca0d5b0fd8d0bd8f82e13e4
Prashant Rana Magar <ranamagar.prashant@gamil.com>
2022-12-19 17:28:40 +0545

    Longest Repeating Sharacter Replacement

---
+++ Strings/longestrepeatingcharacterreplacement.py
@@ -0,0 +1,42 @@
+"""
+Source:https://leetcode.com/problems/longest-repeating-character-replacement/
+
+Question:You are given a string s and an integer k. You can choose any character of the string and change it to any other 
+uppercase English character. You can perform this operation at most k times.Return the length of the longest substring containing 
+the same letter you can get after performing the above operations.
+
+Example 1:
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+
+"""
+
+
+s = "AABABBA"
+k = 1
+
+def characterReplacement(s, k):
+  l = 0
+  result = 0
+  a = {}
+  for r in range(len(s)):
+    if s[r] in  a:
+        a[s[r]] += 1
+    else:
+      a[s[r]] = 1
+    while (r-l+1) - max(a.values()) > k:
+      a[s[l]] -= 1
+      l+=1
+
+    result = max(result, r-l+1)
+
+  return result
+
+print(characterReplacement(s,k))
