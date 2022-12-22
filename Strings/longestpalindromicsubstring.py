8dce4f6188bdeda67fddecbd32a7a31319d03576
Prashant Rana Magar <ranamagar.prashant@gamil.com>
2022-12-22 12:12:24 +0545

    Longest Palindromics String

---
+++ Strings/longestpalindromicsubstring.py
@@ -0,0 +1,48 @@
+"""
+Source:https://leetcode.com/problems/longest-palindromic-substring/description/
+
+Question:Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+Approach:
+1.BruteForce
+1.Expand from the center
+"""
+
+
+def longestPalindrome(self, s):
+    #Approach:Expand from the center.
+    result = ''
+    result_len =0
+
+    for i in range(len(s)):   
+
+        #for odd length
+        left, right=i, i
+        while right<len(s) and left>=0 and s[left]==s[right]:  #check for pallendrome
+            if (right-left+1) > result_len: #check if len of substring is greater than result_len
+                result = s[left:right+1] #update result
+                result_len = right-left+1 #update result len
+        left -= 1
+        right += 1
+
+        #for even length
+        left, right = i, i+1
+        while right<len(s) and left>=0 and s[left]==s[right]:  #check for pallendrome
+            if (right-left+1) > result_len:
+                result = s[left:right+1] 
+                result_len = right-left+1
+        left -= 1
+        right += 1
+        
+    return result
+
+            
--- Strings/validpalindrome.py
+++ Strings/validpalindrome.py
@@ -14,6 +14,7 @@ Explanation: "raceacar" is not a palindrome.
 
 """
 
+
 def palindrome(str):
     outstr = ''
     for char in str:
@@ -22,3 +23,32 @@ def palindrome(str):
     if outstr.lower() == outstr.lower()[::-1]:
         return True
     return False
+
+
+
+
+
+# #Expand from the center
+# import math 
+# def palindrome(str):
+#     # for odd length
+#     mid = math.floor(len(str)/2)
+#     left, right = mid, mid
+
+#     while right<len(str) and left>=0 and str[left]==str[right]:
+#         left -= 1
+#         right +=1
+#         return True
+
+#     #for even length
+#     mid = math.floor(len(str)/2)
+#     left, right = mid, mid+1
+
+#     while right<len(str) and left>=0 and str[left]==str[right]:
+#         left -= 1
+#         right +=1
+#         return True
+
+
+
+
