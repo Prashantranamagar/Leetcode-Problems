99a6ff4d0e110c5204c40e15f5c9950df88d0ef6
Prashant Rana Magar <65164318+Prashantranamagar@users.noreply.github.com>
2024-02-26 16:28:41 +0545

    update with alternate solution

--- Strings/longestcommonprefix.py
+++ Strings/longestcommonprefix.py
@@ -15,17 +15,47 @@ Explanation: There is no common prefix among the input strings.
 
 """
 
+"""
+Approach 1:
+- Sort the given list of string.
+- Store the start index string in first_string and last index string in last_string.
+- store prefix on prifix = ""
+- run loop while i < len(start_string) and i<len(last_string)
+- check condition if start_index[i] != last_string[i] return prefix
+- else increase prefix with prefix+=start_index[i]
+- return prefix outside loop
+"""
 
 
 def longestCommonPrefix(strs):
+    strs.sort()
+    prefix=""
+    first_string = strs[0]
+    last_string = strs[-1]
+    i=0
+    while i<len(first_string) and i<len(last_string):
+        if first_string[i] != last_string[i]:
+            return prefix
+        prefix += first_string[i]
+        i+=1
+    return prefix
+        
+
+
+
+"""
+Approach:2
+"""
+
+# def longestCommonPrefix(strs):
     
-    if len(strs) == 0:
-        return ''
-    lcp = strs[0]
-    for item in strs:
-        for index in range(0, len(lcp)):
-            if index >= len(item) or lcp[index] != item[index]:
-                lcp = lcp[0:index]
-                break
+#     if len(strs) == 0:
+#         return ''
+#     lcp = strs[0]
+#     for item in strs:
+#         for index in range(0, len(lcp)):
+#             if index >= len(item) or lcp[index] != item[index]:
+#                 lcp = lcp[0:index]
+#                 break
             
-    return lcp
+#     return lcp
