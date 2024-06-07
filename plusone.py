8d1fe9b92e2e386603273e59b15fa321061d0fcf
Prashantranamagar <ranamagar.prashant@gmail.com>
2024-06-07 23:39:10 +0545

    plusone

---
+++ plusone.py
@@ -0,0 +1,67 @@
+"""
+Plus One
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits.
+
+Example 1:
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Example 2:
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Example 3:
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+
+"""
+
+
+#Solution 1
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        list_length = len(digits)
+        string = ''
+        for i in range (list_length):
+            j = str(digits[i])
+            string = string + j
+        print(string)
+        string_to_int = int(string) + 1 
+        print(string_to_int)
+        int_to_string = str(string_to_int)
+        result = []
+        for i in range (len(int_to_string)):
+            j = int(int_to_string[i])
+            result.append(j)
+       
+        return result
+    
+
+
+# Solution 2 
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
