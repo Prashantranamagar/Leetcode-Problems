9a95359b96a4e9c4843b949b337011bd15786d95
Prashantranamagar <ranamagar.prashant@gmail.com>
2024-07-16 11:53:08 +0545

    updated min max prob

--- Arrays/minmaxarr.py
+++ Arrays/minmaxarr.py
@@ -5,7 +5,33 @@ Source:https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
 
 """
 
-def minmax(arr):
+
+def min_max(my_list):  # Time Complexity: O(n) space : O(1)
+    max = my_list[0]
+    min = my_list[0]
+    for item in my_list:
+        if item > max:
+            max = item
+        if item < min:
+            min = item
+    return [max, min]
+
+# my_list = [1, 2, 3, 9, 5, 8, 7, 6]
+# print(min_max(my_list))
+
+
+# alternatively
+def minmax_1(my_list):  # Time complexity = O(nlog(n))
+    my_list.sort()  # time complexity of .sort() method is n(logn)
+    max = my_list[-1]
+    min = my_list[0]
+    return max, min
+
+# my_list = [1, 2, 3, 9, 5, 8, 7, 6]
+# print(minmax_1(my_list))
+
+
+def minmax(arr):  # Time complexity = O(n)
     n = len(arr)
     if(n%2 == 0):
         mx = max(arr[0], arr[1])
@@ -22,3 +48,17 @@ def minmax(arr):
             mn = min(mn, arr[i+1])
         i += 2
     return  (mx,mn)
+
+# my_list = [1, 2, 3, 9, 5, 8, 7, 6]
+# minmax(my_list)
+
+
+
+
+
+
+
+
+
+
+
