4bc5410f5b0920e01e64be0360b26ceb9b881a81
Prashantranamagar <ranamagar.prashant@gmail.com>
2024-07-26 20:15:37 +0545

    with subarray

--- Arrays/maximumsubarray.py
+++ Arrays/maximumsubarray.py
@@ -16,13 +16,14 @@ There are two approaches to solve the given problem
         Simply run nested loop over the array
     
     2. Linerar solution 
-        In this approach we find the max(ith item , continue max sum subarray)
-        [-2, 1, -3, 4, -1, 2, 1, -5, 4]   given arry 
-        [-2, 1, -2, 4,  3, 5, 6,  1, 5]   finding the max(ith item , continue max sum subarray)
+    initialize first item as max_item and current_max item
+    loop over the arr or list
+        current_max_item = max(ith item, current_max_item + ith item)
+        if curernt_max_item > max_item:
+            max_item = current_max_item
+    return max_item
 
 """
-
-import math
 
 
 def max_sub_array(nums):
@@ -30,11 +31,36 @@ def max_sub_array(nums):
     max_sum = nums[0]
     length = len(nums)
     for i in range(1, length):
-        current_sum = max(nums[i], current_sum + nums[i])
+        current_sum = max(nums[i], current_sum + nums[i])  
         if current_sum > max_sum:
             max_sum = current_sum
     return max_sum
 
 
+
+
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 print(max_sub_array(nums))
+
+
+
+
+
+def max_subarray_with_subarray(nums):
+    max_so_far = float('-inf')
+    max_ending_here = 0
+    start = end = s = 0
+
+    for i in range(len(nums)):
+        max_ending_here += nums[i]
+
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+            start = s
+            end = i
+
+        if max_ending_here < 0:
+            max_ending_here = 0
+            s = i + 1
+
+    return nums[start:end + 1], max_so_far
